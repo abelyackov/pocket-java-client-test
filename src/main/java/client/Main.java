a112a8def13f3e4c3423b323757c9d4e9d0cf95b
abelyackov <abelyackov@yandex.ru>
2018-11-28 15:57:33 +0300

    Настройка log4j2 на запись логов в различные файлы. Изменения в классах Main, ClientController, HTTPSRequest для отлавливания сообщений

---
+++ src/main/java/client/Main.java
@@ -0,0 +1,78 @@
+package client;
+
+import client.utils.Tray;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    static final Logger mainLogger = LogManager.getLogger(Main.class);
+
+    public static Stage primaryStage;
+    private static BorderPane rootLayout;
+
+    @Override
+    public void start(Stage stage) {
+        primaryStage = stage;
+        primaryStage.setTitle("Pocket desktop client");
+
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/client/images/icon.png")));
+
+        //инициализируем главную сцену
+        initRootLayout();
+
+        //показываем общий вид
+        showOverview();
+        mainLogger.info("Окно приложения запущено");
+
+        //значек в трее
+        Tray tray = new Tray();
+        tray.setTrayIcon();
+
+        primaryStage.setOnCloseRequest(event -> {
+            event.consume();
+            Tray.trayON(primaryStage);
+        });
+    }
+
+    public static void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/client/fxml/RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            Scene scene = new Scene(rootLayout);
+
+            Cursor cursor = Cursor.cursor("HAND");
+            scene.setCursor(cursor);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            mainLogger.error(e);
+        }
+    }
+
+    public static void showOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/client/fxml/LogonView.fxml"));
+            AnchorPane overview = loader.load();
+            rootLayout.setCenter(overview);
+        } catch (IOException e) {
+            mainLogger.error(e);
+        }
+    }
+
+    public static void main(String[] args){launch(args);}
+}
---
+++ src/main/java/client/controller/ClientController.java
@@ -0,0 +1,312 @@
+package client.controller;
+
+import client.Main;
+import client.model.ServerResponse;
+import client.model.formatMsgWithServer.*;
+import client.utils.Connector;
+import client.utils.HTTPSRequest;
+import client.view.ChatViewController;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import database.dao.DataBaseService;
+import database.entity.Message;
+import database.entity.User;
+import javafx.scene.control.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Type;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+
+import static client.utils.Common.showAlert;
+
+public class ClientController {
+    static final Logger controllerLogger = LogManager.getLogger(ClientController.class);
+    private static ClientController instance;
+    private static String token;
+    private ChatViewController chatViewController;
+
+    private User receiver = null;
+    private User sender = null;
+    private Connector conn = null;
+    private List<Long> contactList;
+
+    private DataBaseService dbService;
+
+    public void setChatViewController(ChatViewController chatViewController) {
+        this.chatViewController = chatViewController;
+    }
+
+    private ClientController() {
+    }
+
+    public static ClientController getInstance() {
+        if (instance == null) {
+            instance = new ClientController();
+        }
+        return instance;
+    }
+
+    private void connect(String token) {
+        conn = new Connector(token, ClientController.getInstance());
+    }
+
+    public String getSenderName() {
+        return sender.getAccount_name();
+    }
+
+    public void setReceiver(long receiverId) {
+        this.receiver = dbService.getUser(receiverId);
+        loadChat();
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = dbService.getUserByName(receiver);
+        loadChat();
+    }
+
+    private boolean authentication(String login, String password) {
+        if (!login.isEmpty() && !password.isEmpty()) {
+            String answer = "0";
+            AuthToServer ATS = new AuthToServer(login, password);
+            String reqJSON = new Gson().toJson(ATS);
+            try {
+                answer = HTTPSRequest.authorization(reqJSON);
+            } catch (Exception e) {
+                controllerLogger.error(e);
+                //e.printStackTrace();
+            }
+            if (answer.contains("token")) {
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                AuthFromServer AFS = gson.fromJson(answer, AuthFromServer.class);
+                System.out.println(" answer server " + AFS.token);
+                token = AFS.token;
+                connect(token);
+
+                try {
+                    ServerResponse response = HTTPSRequest.getMySelf(token);
+                    sender = convertJSONToUser(response.getResponseJson());
+                } catch (Exception e) {
+                    controllerLogger.error(e);
+                }
+                sender.setAccount_name(login);
+                synchronizeContactList();
+
+                return true;
+            } else {
+                showAlert("Ошибка авторизации!", Alert.AlertType.ERROR);
+                controllerLogger.error("Ошибка авторизации!");
+            }
+        } else {
+            showAlert("Неполные данные для авторизации!", Alert.AlertType.ERROR);
+            controllerLogger.error("Неполные данные для авторизации!");
+            return false;
+        }
+        return false;
+    }
+
+    private MessageFromServer convertMessageToMFS(String jsonText) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.fromJson(jsonText, MessageFromServer.class);
+    }
+
+    public void receiveMessage(String message) {
+        MessageFromServer mfs = convertMessageToMFS(message);
+        if (!contactList.contains(mfs.getSenderid())) {
+            try {
+                ServerResponse response = HTTPSRequest.getUser(mfs.getSenderid(), token);
+                switch (response.getResponseCode()) {
+                    case 200:
+                        addContact(convertJSONToUser(response.getResponseJson()).getEmail());
+                        break;
+                    case 404:
+                        showAlert("Пользователь не найден", Alert.AlertType.ERROR);//с id: " + mfs.getSenderid() + "
+                        break;
+                    default:
+                        showAlert("Общая ошибка!", Alert.AlertType.ERROR);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        chatViewController.showMessage(mfs.getSender_name(), mfs.getMessage(), mfs.getTimestamp(), true);
+
+        dbService.addMessage(mfs.getReceiver(),
+                mfs.getSenderid(),
+                new Message(mfs.getMessage(),
+                        mfs.getTimestamp()));
+    }
+
+    public void sendMessage(String message) {
+        if (receiver == null) {
+            showAlert("Выберите контакт для отправки сообщения", Alert.AlertType.ERROR);
+            return;
+        }
+        MessageToServer MTS = new MessageToServer(receiver.getUid(), message);
+
+        String jsonMessage = new Gson().toJson(MTS);
+        System.out.println(jsonMessage);
+        conn.getChatClient().send(jsonMessage);
+
+        dbService.addMessage(receiver.getUid(),
+                sender.getUid(),
+                new Message(message, new Timestamp(System.currentTimeMillis()))
+        );
+        chatViewController.showMessage(sender.getAccount_name(), message, new Timestamp(System.currentTimeMillis()), false);
+    }
+
+    private void loadChat() {
+        List<Message> converstation = dbService.getChat(sender, receiver);
+        chatViewController.clearMessageWebView();
+        for (Message message :
+                converstation) {
+            chatViewController.showMessage(message.getSender().getAccount_name(), message.getText(), message.getTime(), false);
+        }
+    }
+
+    public void disconnect() {
+        if (conn != null)
+            conn.getChatClient().close();
+    }
+
+    private Map<String, ContactListFromServer> convertContactListToMap(String jsonText) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type itemsMapType = new TypeToken<Map<String, ContactListFromServer>>() {
+        }.getType();
+        return gson.fromJson(jsonText, itemsMapType);
+    }
+
+    private void synchronizeContactList() {
+        dbService = new DataBaseService();
+        contactList = dbService.getAllUserId();
+
+        try {
+            ServerResponse response = HTTPSRequest.getContacts(token);
+            if (response != null) {
+                Map<String, ContactListFromServer> map = convertContactListToMap(response.getResponseJson());
+                for (Map.Entry<String, ContactListFromServer> entry : map.entrySet()) {
+                    if (!contactList.contains(entry.getValue().getId())) {
+                        User user = new User();
+                        user.setUid(entry.getValue().getId());
+                        user.setAccount_name(entry.getValue().getName());
+                        user.setEmail(entry.getKey());
+
+                        addContactToDB(user);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // проверяем, есть ли наш пользователь в БД
+        User user = dbService.getUser(sender.getUid());
+        if (user == null) {
+            dbService.insertUser(sender);
+        }
+    }
+
+    private User convertJSONToUser(String jsonText) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.fromJson(jsonText, User.class);
+    }
+
+    public void addContact(String contact) {
+        UserToServer cts = new UserToServer(contact);
+        String requestJSON = new Gson().toJson(cts);
+
+        try {
+            ServerResponse response = HTTPSRequest.addContact(requestJSON, token);
+            switch (response.getResponseCode()) {
+                case 201:
+                    showAlert("Контакт " + contact + " успешно добавлен", Alert.AlertType.INFORMATION);
+                    addContactToDB(convertJSONToUser(response.getResponseJson()));
+                    if (chatViewController != null) chatViewController.fillContactListView();
+                    break;
+                case 404:
+                    showAlert("Пользователь с email: " + contact + " не найден", Alert.AlertType.ERROR);
+                    break;
+                case 409:
+                    showAlert("Пользователь " + contact + " уже есть в списке ваших контактов", Alert.AlertType.ERROR);
+                    break;
+                default:
+                    showAlert("Общая ошибка!", Alert.AlertType.ERROR);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addContactToDB(User contact) {
+        dbService.insertUser(new User(contact.getUid(), contact.getAccount_name(), contact.getEmail()));
+        contactList.add(contact.getUid());
+    }
+
+    public void proceedRegister(String login, String password, String email) {
+        String requestJSON = "{" +
+                "\"account_name\": \"" + login + "\"," +
+                "\"email\": \"" + email + "\"," +
+                "\"password\": \"" + password + "\"" +
+                "}";
+        try {
+            int responseCode = HTTPSRequest.registration(requestJSON);
+            if (responseCode == 201) {
+                showAlert("Вы успешно зарегистрированы", Alert.AlertType.INFORMATION);
+                controllerLogger.info(login + " успешно зарегистрирован.");
+            } else {
+                showAlert("Ошибка регистрации, код: " + responseCode, Alert.AlertType.ERROR);
+                controllerLogger.error("Ошибка регистрации, ответ от сервера: " + responseCode);
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+            controllerLogger.error(e);
+        }
+    }
+
+    public boolean proceedLogIn(String login, String password) {
+        return authentication(login, password);
+    }
+
+    public List<String> getAllUserNames() {
+        return dbService.getAllUserNames();
+    }
+
+    public void dbServiceClose() {
+        dbService.close();
+    }
+
+    public String proceedRestorePassword(String email) {
+        String answer = "0";
+        String requestJSON = "{" +
+                "\"email\": \"" + email + "\"" +
+                "}";
+        try {
+            answer = HTTPSRequest.restorePassword(requestJSON);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return answer;
+    }
+
+    public String proceedChangePassword(String email, String codeRecovery, String password) {
+        String answer = "0";
+        String requestJSON = "{" +
+                "\"email\": \"" + email + "\"," +
+                "\"code\": \"" + codeRecovery + "\"," +
+                "\"password\": \"" + password + "\"" +
+                "}";
+        try {
+            answer = HTTPSRequest.changePassword(requestJSON);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return answer;
+    }
+}
